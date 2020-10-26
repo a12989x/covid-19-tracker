fac80ab3184b2c0fc984befe9d48ca40a8628152
a12989x <alexis12989x@gmail.com>
2020-10-26 17:45:09 -0600

    Set up Graph component

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Line } from 'react-chartjs-2';
+
+import { CovidContext } from '../contexts/CovidContext';
+
+import options from '../chartOptions';
 
 const Graph = () => {
+    const { graphData } = useContext(CovidContext);
+
     return (
         <div>
-            <p>Graph Component</p>
+            {graphData?.length > 0 && (
+                <Line
+                    data={{
+                        datasets: [
+                            {
+                                backgroundColor: 'rgba(204, 16, 52, 0.5)',
+                                borderColor: '#CC1034',
+                                data: graphData,
+                            },
+                        ],
+                    }}
+                    options={options}
+                />
+            )}
         </div>
     );
 };
