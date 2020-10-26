e86eedd0a489acd04390d0ed18e5403d8d4a144e
a12989x <alexis12989x@gmail.com>
2020-10-26 12:01:18 -0600

    Add CovidContext to can access

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import CovidContextProvider from './contexts/CovidContext';
+
 import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <CovidContextProvider>
+            <App />
+        </CovidContextProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
