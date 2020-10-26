4a475b6e7fd3d63c653733adf9dfd09460a1d935
a12989x <alexis12989x@gmail.com>
2020-10-26 12:00:50 -0600

    Import CovidContext and Select component to show countries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Select from 'react-select';
+
+import { CovidContext } from './contexts/CovidContext';
 
 const App = () => {
+    const { countries, onCountryChange } = useContext(CovidContext);
+
     return (
         <div className='app'>
             <p>App Component</p>
+            <Select
+                options={countries}
+                defaultValue={countries[217]}
+                value={countries[217]}
+                onChange={onCountryChange}
+            />
         </div>
     );
 };
