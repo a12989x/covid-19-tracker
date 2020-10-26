363d9cdff379a120a724cdcc4b4c11d903ce1f76
a12989x <alexis12989x@gmail.com>
2020-10-26 13:53:03 -0600

    Set up Table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { CovidContext } from '../contexts/CovidContext';
 
 const Table = () => {
+    const { tableData } = useContext(CovidContext);
+
     return (
         <div>
-            <p>Table Component</p>
+            <h2>Live cases by Country</h2>
+            {tableData?.map(({ country, cases }) => (
+                <tr>
+                    <td>{country}</td>
+                    <td>
+                        <strong>{cases}</strong>
+                    </td>
+                </tr>
+            ))}
         </div>
     );
 };
