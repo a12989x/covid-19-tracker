6187c75a25ee1d5270fa7b51091a8ff2f4d043a1
a12989x <alexis12989x@gmail.com>
2020-10-29 11:14:53 -0600

    Set up Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Map as LeafletMap, TileLayer } from 'react-leaflet';
+
+import { CovidContext } from '../contexts/CovidContext';
+
+import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
+    const {
+        mapCenter,
+        mapZoom,
+        mapCountries,
+        casesType,
+        showDataOnMap,
+    } = useContext(CovidContext);
+
     return (
-        <div>
-            <p>Map Component</p>
+        <div className='map'>
+            <LeafletMap center={mapCenter} zoom={mapZoom}>
+                <TileLayer
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {showDataOnMap(mapCountries, casesType)}
+            </LeafletMap>
         </div>
     );
 };
